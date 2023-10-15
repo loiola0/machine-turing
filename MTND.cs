14dcedb32f045e1b7021bb9cb996017b6812c357
loiola0 <victorloiola@alu.ufc.br>
2023-10-15 17:42:53 -0300

    primeira versão

---
+++ MTND.cs
@@ -0,0 +1,74 @@
+﻿namespace MachineTuring
+{
+    public class MTND
+    {
+        private HashSet<string> States { get; set; }
+
+        private HashSet<char> Alphabet { get; set; }
+
+        private Dictionary<Tuple<string, char>, List<Tuple<string, char, char>>> TransitionFunction;
+
+        private string StartState { get; set; }
+
+        private HashSet<string> AcceptStates { get; set; }
+
+        public MTND(HashSet<string> states, HashSet<char> alphabet, Dictionary<Tuple<string, char>, List<Tuple<string, char, char>>> transitionFunction, string startState, HashSet<string> acceptStates)
+        {
+            States = states;
+            
+            Alphabet = alphabet;
+            
+            TransitionFunction = transitionFunction;
+            
+            StartState = startState;
+            
+            AcceptStates = acceptStates;
+        }
+
+        public Tuple<string, string, int> Simulate(string inputString)
+        {
+            string currentState = StartState;
+            
+            char[] tape = new char[inputString.Length + 2];
+            
+            int headPosition = 1;
+
+            for (int i = 0; i < tape.Length; i++)
+            {
+                tape[i] = ' ';
+            }
+
+            inputString.CopyTo(0, tape, 1, inputString.Length);
+
+            while (!AcceptStates.Contains(currentState))
+            {
+                char currentSymbol = tape[headPosition];
+
+                if (TransitionFunction.TryGetValue(Tuple.Create(currentState, currentSymbol), out var transitions))
+                {
+                    foreach (var transition in transitions)
+                    {
+                        tape[headPosition] = transition.Item2;
+
+                        if (transition.Item3 == 'R')
+                            headPosition++;
+
+                        else if (transition.Item3 == 'L')
+                            headPosition--;
+
+                        currentState = transition.Item1;
+
+                        if (AcceptStates.Contains(currentState))
+                            break;
+                    }
+                }
+                else
+                    break; 
+
+            }
+
+            return Tuple.Create(currentState, new string(tape), headPosition);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using MachineTuring;
+
+HashSet<string> states = new HashSet<string> { "q0", "q1", "q2" };
+
+HashSet<char> alphabet = new HashSet<char> { '0', '1' };
+
+var transitionFunction = new Dictionary<Tuple<string, char>, List<Tuple<string, char, char>>>
+{
+            { Tuple.Create("q0", '0'), new List<Tuple<string, char, char>> { Tuple.Create("q0", '0', 'R') } },
+            { Tuple.Create("q0", '1'), new List<Tuple<string, char, char>> { Tuple.Create("q0", '1', 'R'), Tuple.Create("q1", '1', 'R') } },
+            { Tuple.Create("q1", '1'), new List<Tuple<string, char, char>> { Tuple.Create("q2", '1', 'R') } }
+};
+
+string startState = "q0";
+
+HashSet<string> acceptStates = new HashSet<string> { "q2" };
+
+MTND mtnd = new MTND(states, alphabet, transitionFunction, startState, acceptStates);
+
+string[] inputStrings = { "0111", "0011", "111", "110" };
+
+foreach (var inputString in inputStrings)
+{
+    var result = mtnd.Simulate(inputString);
+    Console.WriteLine("Entrada: " + inputString);
+    Console.WriteLine("Estado Final: " + result.Item1);
+    Console.WriteLine("Fita Final: " + result.Item2);
+    Console.WriteLine("Posição da Cabeça de Leitura: " + result.Item3);
+    Console.WriteLine("---");
+}
